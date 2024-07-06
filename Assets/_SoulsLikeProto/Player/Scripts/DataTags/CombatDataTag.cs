e35c1a16821dc9699eacea160675bc2aa6b85cb5
Jared Goronkin <jgoronkin@gmail.com>
2024-07-06 17:35:05 -0400

    Added interaction, health, and death

---
+++ Assets/_SoulsLikeProto/Player/Scripts/DataTags/CombatDataTag.cs
@@ -0,0 +1,11 @@
+using Mosaic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatDataTag : DataTag
+{
+    public int Health = 10;
+    public int Stamina;
+    public int WeaponDamage = 4;
+}
