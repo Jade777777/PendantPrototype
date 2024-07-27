5f1854f8ef33dd40cbbf8a71589c73ea5f980618
Jared Goronkin <jgoronkin@gmail.com>
2024-07-26 22:51:07 -0400

    Health Pots Use Modifiers

--- Assets/_SoulsLikeProto/Player/Scripts/States/HealState.cs
+++ Assets/_SoulsLikeProto/Player/Scripts/States/HealState.cs
@@ -1,5 +1,6 @@
 
 using UnityEngine;
+using Mosaic;
 namespace SoulsLike
 {
     public class HealState : BaseSoulState
@@ -8,7 +9,7 @@ namespace SoulsLike
         [SerializeField]
         float _attackTime = 0.5f;
         [SerializeField]
-        int _healing = 4;
+        Modifier _modifier;
         [SerializeField] 
         BehaviorInputType _nextInput;
 
@@ -26,10 +27,8 @@ namespace SoulsLike
         {
             if (DataTags.GetTag<CombatDataTag>().HealthPots >= 1)
             {
-                DataTags.GetTag<CombatDataTag>().Health--;
-                DataTags.GetTag<CombatDataTag>().Health = 
-                    Mathf.Clamp(DataTags.GetTag<CombatDataTag>().Health + _healing, 0, DataTags.GetTag<CombatDataTag>().MaxHealth);
-                Debug.Log("Healed! " + DataTags.GetTag<CombatDataTag>().HealthPots + " health pots remaining");
+                DataTags.GetTag<CombatDataTag>().HealthPots--;
+                Core.Modifiers.ApplyModifier(_modifier,Core);
             }
             else
             {
